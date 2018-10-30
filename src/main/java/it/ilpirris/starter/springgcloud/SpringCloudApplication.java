d5a675cf9006e778ecf553adaf8f20cf5d217a23
Mario Pirris <mario.pirris@infocamere.it>
2018-10-30 10:37:54 +0100

    ready to fork

---
+++ src/main/java/it/ilpirris/starter/springgcloud/SpringCloudApplication.java
@@ -0,0 +1,19 @@
+package it.ilpirris.starter.springgcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringCloudApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringCloudApplication.class);
+	}
+}
